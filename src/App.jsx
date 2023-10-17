b543a37dc246799504f3aec2af7baad25ab43060
vunontay <hoangvu200202@gmail.com>
2023-10-17 20:43:05 +0700

    setup success

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,54 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from "./redux/counter/counterSlice";
-import styles from './styles/Counter.module.css';
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login/Login';
+import RegisterPage from './pages/register/Register';
+import ContactPage from './pages/contact/Contact';
+import { Outlet } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
+
+const Layout =()=>{
+  return(
+    <div className='layout-app'>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 export default function App() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      errorElement: <div>404 not found</div>,
 
-  const incrementValue = Number(incrementAmount) || 0;
-
+      children: [
+        {index: true, element:<Home/>},
+        {
+          path:'contact',
+          element: <ContactPage/>
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <LoginPage/>,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage/>,
+    },
+  ]);
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+   <>
+   <RouterProvider router={router} />
+   
+   </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import App from './App'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App/>
     </Provider>
   </React.StrictMode>
 )
